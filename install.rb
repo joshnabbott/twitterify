07db9d6141d444bcbe8e8f09c4156d488861a569
Joshua Abbott <joshnabbott@joshua-abbotts-macbook-pro.local>
2009-05-09 21:45:00 -0700

    Install/uninstall hooks

--- install.rb
+++ install.rb
@@ -1,2 +1,2 @@
-File.copy("#{File.dirname(__FILE__)}/example/twitterify.yml","#{RAILS_ROOT}/config/twitterify.yml")
+File.copy("#{File.dirname(__FILE__)}/example/twitterify.yml","#{RAILS_ROOT}/config")
 puts File.read("#{File.dirname(__FILE__)}/README.rdoc")
--- uninstall.rb
+++ uninstall.rb
@@ -1,3 +1,2 @@
-require 'fileutils'
 config_file = "#{RAILS_ROOT}/config/twitterify.yml"
-FileUtils.rm(config_file) if File.exist?(config_file)
+File.delete(config_file) if File.exists?(config_file)
